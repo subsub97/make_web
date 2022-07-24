b935b04dc9f3752b6f698063b749a78be672d25d
HO <godqhr721@gmail.com>
2022-07-24 20:04:53 +0900

    practing making a stopwatch

---
+++ StopWatch/script.js
@@ -0,0 +1,31 @@
+let min=0;
+let sec=0;
+let ten_Msec=0;
+
+const appendTens = document.getElementById('ten_Msec');
+const appendMin = document.getElementById('min');
+const appendSec = document.getElementById('sec');
+const buttonStart = document.getElementById('bt_start');
+const buttonStop = document.getElementById('bt_stop');
+const buttonReset = document.getElementById('bt_reset');
+
+buttonStart.onclick = function(){
+  setInterval(operateTimer,10)
+}
+
+//10ms 마다 시간에 대한 숫자가 증가한다.
+function operateTimer(){
+  ten_Msec++;
+  appendTens.textContent = ten_Msec > 9 ? ten_Msec : '0' + ten_Msec;
+  if(ten_Msec>99){
+    sec++;
+    appendSec.textContent = sec > 9 ? sec : '0' + sec;
+    ten_Msec=0;
+  }
+  if(sec>59){
+    min++;
+    appendMin.textContent = min > 9 ? min : '0' + min;
+    sec=0;
+    appendSec.textContent="00"
+  }
+}
