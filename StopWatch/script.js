63d910f13b8eddf832eebff85c975d552b359589
HO <godqhr721@gmail.com>
2022-07-30 16:39:39 +0900

    add stop and reset

--- StopWatch/script.js
+++ StopWatch/script.js
@@ -1,6 +1,6 @@
-let min=0;
-let sec=0;
-let ten_Msec=0;
+let min = 0;
+let sec = 0;
+let ten_Msec = 0;
 
 const appendTens = document.getElementById('ten_Msec');
 const appendMin = document.getElementById('min');
@@ -8,24 +8,39 @@ const appendSec = document.getElementById('sec');
 const buttonStart = document.getElementById('bt_start');
 const buttonStop = document.getElementById('bt_stop');
 const buttonReset = document.getElementById('bt_reset');
+let intervalId;
 
-buttonStart.onclick = function(){
-  setInterval(operateTimer,10)
+
+buttonStart.onclick = function () {
+  clearInterval(intervalId)
+  intervalId = setInterval(operateTimer, 10)
+}
+
+buttonStop.onclick = function () {
+  clearInterval(intervalId)
+}
+
+buttonReset.onclick = function () {
+  clearInterval(intervalId)
+  ten_Msec = 0; sec = 0; min = 0;
+  appendTens.textContent = "00"
+  appendSec.textContent = "00"
+  appendMin.textContent = "00"
 }
 
 //10ms 마다 시간에 대한 숫자가 증가한다.
-function operateTimer(){
+function operateTimer() {
   ten_Msec++;
   appendTens.textContent = ten_Msec > 9 ? ten_Msec : '0' + ten_Msec;
-  if(ten_Msec>99){
+  if (ten_Msec > 99) {
     sec++;
     appendSec.textContent = sec > 9 ? sec : '0' + sec;
-    ten_Msec=0;
+    ten_Msec = 0;
   }
-  if(sec>59){
+  if (sec > 59) {
     min++;
     appendMin.textContent = min > 9 ? min : '0' + min;
-    sec=0;
-    appendSec.textContent="00"
+    sec = 0;
+    appendSec.textContent = "00"
   }
 }
