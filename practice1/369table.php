05e5dd7071f2c36e156e0c29c733b49b7dff9a97
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2022-11-17 19:30:36 +0900

    modify background color that matchs special number

--- practice1/369table.php
+++ practice1/369table.php
@@ -3,21 +3,27 @@
   <head>
     <meta charset="utf-8">
     <style>
-    	table { border-collapse:collapse; width:600px; }
-    	td { border: solid 1px gray; text-align: center; padding:5px; }
+    	table { border-collapse:collapse; width:600px; border: solid 3px black;}
+    	td { border: solid 1px black; text-align: center; padding:5px; }
 
       #third {
         background-color: cyan;
         color: red;
+        font-weight: bold;
       }
       #mode{
         background-color: yellow;
         color:blue;
+        font-weight: bold;
+      }
+      p{
+        font-size: 20px;
+        font-weight: bold;
       }
     </style>
   </head>
   <body>
-    <?php echo "<h3>3-6-9<h3>"?>
+    <?php echo "<p>3-6-9 테이블</p> "?>
 
     <table>
     <?php
@@ -25,6 +31,7 @@
     	for ($row = 1; $row <= 100; $row++) {
     		echo "<tr>";
     		for ($col = 0; $col <= 9; $col++) {
+          $check_num = (int)($num/10);
           // 3의 배수인 경우 백그라운드를 다르게 하기 위한 ID부여
           if($num % 3 ==0){
     			echo "<td id = 'third'>$num</td>";
@@ -33,7 +40,14 @@
           else if($num % 10 == 3 || $num % 10 == 6 || $num % 10 == 9 ){
             echo "<td id = 'mode'>$num</td>";
           }
-
+          // 10의 자리가 3,6,9인 경우 와 아닌경우 구분하기
+          else if ($check_num % 10 == 3 || $check_num%10 == 6 || $check_num%10 == 9){
+             echo "<td id = 'mode'>$num</td>";
+          }
+          // 100의 자리 수가 3 6 9 인경우
+          else if((int)($num/100) == 3 || (int)($num/100) == 6 || (int)($num/100) == 9){
+            echo "<td id = 'mode'>$num</td>";
+          }
           else {
           echo "<td>$num</td>";
           }
